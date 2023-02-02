dbf07bd7225013473339a9a6327f565d2188b9b5
Cindy <telcindy59@gmail.com>
2023-02-02 11:08:07 +0100

    ajout du header

---
+++ react-maquette/mon-projet-react/src/Components/Header.jsx
@@ -0,0 +1,33 @@
+
+
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="title">
+                <h1>
+                    <span className="block">Découvrez notre </span>
+                    <span className="block skew">menu.</span>
+                </h1>
+               
+            </div>
+            <div className="nav">
+
+                <ul className="link">
+                    <li>
+                        <a href="#" className="bg">Tout</a>
+                    </li>
+                    <li>
+                        <a href="#">Viandes</a>
+                    </li>
+                    <li>
+                        <a href="#">Légumes</a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
